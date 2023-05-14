a7ee31cce2b4bfcd081034894c3d9a9a2eb21663
bijuli74 <rust_bondcr007@yahoo.com>
2023-05-14 13:19:53 -0400

    Modified test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -195,3 +195,8 @@ class TestAccountService(TestCase):
         # Delete an account that does not exist
         response = self.client.delete(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
