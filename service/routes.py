09129117fba9b2a03b39b4e1b5182a5fa3dcb684
bijuli74 <rust_bondcr007@yahoo.com>
2023-05-14 08:36:46 -0400

    Added CRUD operations

--- service/routes.py
+++ service/routes.py
@@ -51,8 +51,8 @@ def create_accounts():
     account.create()
     message = account.serialize()
     # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
+    location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    # location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -61,34 +61,59 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
-
+@app.route("/accounts", methods=["GET"])
+def get_accounts():
+    """ Fetches all the Accounts """
+    accounts = list(Account.all())
+    return make_response(jsonify(accounts), status.HTTP_200_OK)
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """ Read details of an Account with account id """ 
+    account = Account.find(account_id)
+    if account is None:
+        abort(status.HTTP_404_NOT_FOUND, "Account not found")
+    
+    return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """ Update an account using account id """
+    account = Account.find(account_id)
+    if account is None:
+        abort(status.HTTP_404_NOT_FOUND, "Account not found")
+    
+    check_content_type("application/json")
+    account.deserialize(request.get_json())
+    account.update()
 
+    return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
-
+@app.route("/accounts/<int:id>", methods=["DELETE"])
+def delete_an_account(account_id):
+    """ Delete an account with an account id """
+    account = account.find(account_id)
+    if account is None:
+        make_response("", status.HTTP_204_NO_CONTENT)
+    
+    account.delete()
+    return make_response("", status.HTTP_204_NO_CONTENT)
+    
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
